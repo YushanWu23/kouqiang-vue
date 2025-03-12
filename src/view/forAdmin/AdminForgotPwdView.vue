7b52070353ae22ce75a29e803de8e75d7270230e
YushanWu23 <20211120042@mail.ynu.edu.cn>
2025-03-12 19:25:38 +0800

    管理员登陆

---
+++ src/view/forAdmin/AdminForgotPwdView.vue
@@ -0,0 +1,271 @@
+<template>
+    <div class="background">
+        <img src="../../assets/img-new/背景图3.png">
+    </div>
+    <div class="wrapper">
+        <div class="login-container">
+            <div class ="head">
+                <div class = "headText">
+                    忘记密码(管理端)
+                </div>
+            </div>
+            <div class="emailNum">
+                <div class="Text1">
+                    邮&nbsp;箱&nbsp;号：
+                </div>
+                <div class="Text2">
+                    <input type="text" v-model="admin.adminId" placeholder="邮&nbsp;箱&nbsp;号">
+                </div>
+            </div>
+            <div class="emailCode">
+                <div class="Text1">
+                    验&nbsp;证&nbsp;码：
+                </div>
+                <div class="Text2">
+                    <input type="text" v-model="emailcode" placeholder="验&nbsp;证&nbsp;码">
+                    <button @click="sendEmailCode">获取验证码</button>
+                </div>
+            </div>
+            <div class="pwd">
+                <div class="Text1">
+                    新&nbsp;密&nbsp;码：
+                </div>
+                <div class="Text2">
+                    <input type="password" v-model="admin.password" placeholder="新&nbsp;密&nbsp;码">
+                </div>
+            </div>
+            <div class="rePwd">
+                <div class="Text1">
+                    确认密码：
+                </div>
+                <div class="Text2">
+                    <input type="password" v-model="rePassword" placeholder="确认密码">
+                </div>
+            </div>
+            <div class="passwordSave">
+                <button @click="passwordSave">保存</button>
+            </div>
+            <div class="login">
+                <span>已想起密码？</span>
+                <button @click="login">去登陆</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import BottomComponent from "@/components/basic/TopComponent.vue";
+import {axiosInstance, useStore} from "@/main";
+import {useRouter} from "vue-router";
+import {storeToRefs} from "pinia";
+
+const router = useRouter()
+const store = useStore()
+const { admin } = storeToRefs(store)
+const rePassword = ref('')
+const emailcode = ref('')
+function passwordSave() {
+    if (!admin.value.adminId) {
+        alert('邮箱号不能为空！');
+        return;
+    }
+    if (!emailcode) {
+        alert('验证码不能为空！');
+        return;
+    }
+    if (!admin.value.password) {
+        alert('新密码不能为空！');
+        return;
+    }
+    if (admin.value.password !== rePassword.value) {
+        alert("新密码和确认密码不匹配！");
+        return;
+    }
+    axiosInstance.post('/admin/passwordForget',{
+        adminId : admin.value.adminId,
+        newPassword: admin.value.password,
+        emailCode: emailcode.value,
+    },{
+        headers : {
+            "Content-Type" : "application/x-www-form-urlencoded"
+        },
+    }).then((response)=>{
+        if (!response.data){
+            alert('保存失败，请重试！');
+            return;
+        }
+        alert('保存成功！');
+        router.push('/adminInfo')
+    }).catch(error=>{
+        console.error(error);
+    });
+}
+function login() {
+    router.go(-1)
+}
+function sendEmailCode(){
+    if (!admin.value.adminId) {
+        alert('邮箱号不能为空！');
+        return;
+    }
+
+    axiosInstance.post('/admin/sendForgetPasswordEmailCode', {
+        adminId: admin.value.adminId
+    },{
+        headers : {
+            "Content-Type" : "application/x-www-form-urlencoded"
+        },
+    }).then(response => {
+        if (response.data === 1) {
+            alert('验证码已发送，请注意查收！');
+        } else {
+            alert('验证码发送失败，请稍后重试！');
+        }
+    }).catch(error => {
+        console.error(error);
+        alert('验证码发送失败，请稍后重试！');
+    });
+}
+</script>
+
+<style scoped>
+.background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1; /* 确保背景图在最底层 */
+}
+
+.background img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* 保持图片比例并填充整个区域 */
+}
+.wrapper {
+    display: flex;
+    flex-direction: column; /* 子元素水平排列 */
+    justify-content: center;
+    align-items: flex-end;
+    height: 105vh; /* 高度占满整个视口 */
+    box-sizing: border-box; /* 包含 padding */
+}
+.login-container {
+    background-color: white;
+    padding: 20px 30px 20px;
+    border-radius: 8px; /* 圆角边框 */
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 添加阴影效果 */
+    width: 400px;
+}
+.wrapper .head {
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+}
+.headText {
+    font-size: 24px;
+    font-weight: bold;
+    display: flex;
+    color: #555555;
+}
+.emailNum, .pwd, .rePwd, .name, .sex, .emailCode{
+    margin-bottom: 12px;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+}
+.regist{
+
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+}
+.Text1 {
+    font-size: 16px;
+    width: 25%;
+}
+.Text2 input {
+    width: 125%;
+    padding: 5px 5px 5px 10px;
+    margin-left: 5px;
+    border: 1px solid #ccc; /* 输入框边框 */
+    border-radius: 4px; /* 输入框圆角 */
+}
+.wrapper .sex .Text3{
+    width: 135%;
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: left;
+}
+.wrapper .sex .Text3 input[type="radio"] {
+    margin-right: 10px;
+    margin-left: 10px;
+}
+.emailCode .Text2 input {
+    width: 60%;
+    padding: 5px 5px 5px 10px;
+    margin-left:15px;
+    border: 1px solid #ccc; /* 输入框边框 */
+    border-radius: 4px; /* 输入框圆角 */
+}
+.emailCode .Text2 button {
+    width: 34%;
+    padding: 5px;
+    background-color: #007bff;
+    color: white;
+    border: none; /* 按钮无边框 */
+    border-radius: 4px;
+    cursor: pointer; /* 鼠标悬停时显示手型 */
+}
+
+.emailCode .Text2 button:hover {
+    background-color: #0056b3; /* 鼠标悬停时的背景颜色 */
+}
+.passwordSave button {
+    width: 100%;
+    padding: 10px;
+    margin-top: 2px;
+    background-color: #007bff;
+    color: white;
+    border: none; /* 按钮无边框 */
+    border-radius: 4px;
+    cursor: pointer; /* 鼠标悬停时显示手型 */
+}
+
+.passwordSave button:hover {
+    background-color: #0056b3; /* 鼠标悬停时的背景颜色 */
+}
+.login {
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+}
+.login span {
+    font-size: 15px;
+    color: #666;
+    width: 30%;
+}
+.login button {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    background-color: transparent; /* 透明背景 */
+    color: #007bff;
+    border: none; /* 无边框 */
+    cursor: pointer; /* 鼠标悬停时显示手型 */
+    padding-left: 5px;
+    font-size: 15px;
+}
+.login button:hover {
+    text-decoration: underline; /* 鼠标悬停时下划线 */
+}
+
+</style>
---
+++ src/view/forAdmin/AdminLoginView.vue
@@ -0,0 +1,228 @@
+<template>
+    <div class="background">
+        <img src="../../assets/img-new/背景图3.png">
+    </div>
+    <div class="wrapper">
+        <div class="login-container">
+            <div class="user">
+                <span>返回</span>
+                <button @click="userLogin">用户端</button>
+            </div>
+            <div class ="head">
+                <div class = "headText">
+                    管理员登陆
+                </div>
+            </div>
+            <div class="EmailNum">
+                <div class="Text1">
+                    邮箱号：
+                </div>
+                <div class="Text2">
+                    <input type="text" v-model="admin.adminId" placeholder="邮箱号">
+                </div>
+            </div>
+            <div class="pwd">
+                <div class="Text1">
+                    密&nbsp;&nbsp;&nbsp;&nbsp;码：
+                </div>
+                <div class="Text2">
+                    <input type="password" v-model="admin.password" placeholder="密&nbsp;&nbsp;&nbsp;&nbsp;码">
+                </div>
+            </div>
+            <div class="login">
+                <button @click="login">登陆</button>
+            </div>
+            <div class="bottom-links">
+                <div class="register">
+                </div>
+                <div class="forgot-password">
+                    <button @click="forgotPassword">忘记密码</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import BottomComponent from "@/components/basic/TopComponent.vue";
+import {axiosInstance, useStore} from "@/main";
+import {useRouter} from "vue-router";
+import {storeToRefs} from "pinia";
+
+const router = useRouter()
+const store = useStore()
+const { admin } = storeToRefs(store)
+function login() {
+    if (!admin.value.adminId) {
+        alert('邮箱号不能为空！');
+        return;
+    }
+    if (!admin.value.password) {
+        alert('密码不能为空！');
+        return;
+    }
+    axiosInstance.post('/admin/login',{
+        adminId : admin.value.adminId,
+        pwd: admin.value.password,
+    },{
+        headers : {
+            "Content-Type" : "application/x-www-form-urlencoded"
+        }
+    }).then(response=>{
+        if (!response.data){
+            alert('用户名或密码不正确！');
+            return
+        }
+        sessionStorage.setItem("token",response.data)
+        alert('登录成功！');
+        /*router.go(-1)*/
+        router.push({
+            path : "/adminInfo"
+        })
+    }).catch(error=>{
+        console.error(error);
+    });
+}
+function regist() {
+    router.push({
+        path : "/regist"
+    })
+}
+function forgotPassword() {
+    router.push({
+        path : "/adminForgotPwd"
+    })
+}
+function userLogin() {
+    router.push({
+        path : "/login"
+    })
+}
+</script>
+<style scoped>
+.background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1; /* 确保背景图在最底层 */
+}
+
+.background img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* 保持图片比例并填充整个区域 */
+}
+
+/* 设置登录框的样式 */
+.wrapper {
+    display: flex;
+    flex-direction: column; /* 子元素水平排列 */
+    justify-content: center;
+    align-items: flex-end;
+    height: 100vh; /* 高度占满整个视口 */
+    box-sizing: border-box; /* 包含 padding */
+}
+.login-container {
+    background-color: white;
+    padding: 30px;
+    border-radius: 8px; /* 圆角边框 */
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 添加阴影效果 */
+    width: 400px;
+}
+.head {
+    margin-top: 10px;
+    margin-bottom: 25px;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+}
+.EmailNum, .pwd, .login {
+    margin-bottom: 25px;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+}
+
+.headText {
+    font-size: 24px;
+    font-weight: bold;
+    display: flex;
+    color: #555555;
+}
+
+.Text1 {
+    font-size: 16px;
+    width: 20%;
+}
+
+.Text2 input {
+    width: 135%;
+    padding: 5px 5px 5px 10px;
+    margin-left: 5px;
+    border: 1px solid #ccc; /* 输入框边框 */
+    border-radius: 4px;
+}
+
+.login button {
+    width: 100%;
+    padding: 10px;
+    background-color: #007bff;
+    color: white;
+    border: none; /* 按钮无边框 */
+    border-radius: 4px; /* 按钮圆角 */
+    cursor: pointer; /* 鼠标悬停时显示手型 */
+}
+
+.login button:hover {
+    background-color: #0056b3; /* 鼠标悬停时的背景颜色 */
+}
+
+.bottom-links {
+    display: flex;
+    align-items: center;
+}
+
+.bottom-links .register {
+    display: flex; /* 使用 flex 布局 */
+    align-items: center; /* 垂直居中 */
+    width: 50%;
+}
+
+.bottom-links .forgot-password button {
+    background-color: transparent; /* 透明背景 */
+    color: #007bff;
+    border: none; /* 无边框 */
+    cursor: pointer; /* 鼠标悬停时显示手型 */
+    font-size: 15px;
+    margin-left: 100px;
+}
+
+.bottom-links .forgot-password button:hover {
+    text-decoration: underline; /* 鼠标悬停时下划线 */
+}
+.user {
+    display: flex; /* 使用 flex 布局 */
+    align-items: center; /* 垂直居中 */
+}
+
+.user span {
+    margin-right: 3px;
+    font-size: 13px;
+    color: #666;
+}
+.user button {
+    background-color: transparent; /* 透明背景 */
+    color: #007bff;
+    border: none; /* 无边框 */
+    cursor: pointer; /* 鼠标悬停时显示手型 */
+    font-size: 13px;
+}
+.user button:hover {
+    text-decoration: underline; /* 鼠标悬停时下划线 */
+}
+</style>
